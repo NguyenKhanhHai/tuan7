73eb6089a02ba31d973304d89fae2082675a3267
nguyenlyluat-star <nguyenlyluat@gmail.com>
2025-10-20 20:45:12 +0700

    Bai cua Luật hoàn thành

--- SachTieuThuyet.java
+++ SachTieuThuyet.java
@@ -2,7 +2,8 @@ public class SachTieuThuyet extends Sach implements IKiemKe {
     private String theLoai;
     private boolean laSachSeries;
 
-    public SachTieuThuyet(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong, double giaCoBan, String theLoai, boolean laSachSeries) {
+    public SachTieuThuyet(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong,
+                          double giaCoBan, String theLoai, boolean laSachSeries) {
         super(maSach, tieuDe, tacGia, namXuatBan, soLuong, giaCoBan);
         this.theLoai = theLoai;
         this.laSachSeries = laSachSeries;
@@ -10,19 +11,9 @@ public class SachTieuThuyet extends Sach implements IKiemKe {
 
     @Override
     public double tinhGiaBan() {
-        double phuThu = laSachSeries ? 15000 : 0;
-        return getGiaCoBan() + phuThu;
+        return getGiaCoBan() + (laSachSeries ? 15000 : 0);
     }
 
-    @Override
-    public String toString() {
-        return super.toString() + "\n" +
-               "Thể loại: " + theLoai + "\n" +
-               "Là sách series: " + (laSachSeries ? "Có" : "Không") + "\n" +
-               "Giá bán ước tính: " + tinhGiaBan() + " VNĐ";
-    }
-
-    // Implement IKiemKe
     @Override
     public boolean kiemTraTonKho(int soLuongToiThieu) {
         return getSoLuong() >= soLuongToiThieu;
@@ -30,6 +21,14 @@ public class SachTieuThuyet extends Sach implements IKiemKe {
 
     @Override
     public void capNhatViTri(String viTriMoi) {
-        System.out.println("Đã chuyển sách [" + getTieuDe() + "] đến khu vực: " + viTriMoi);
+        System.out.println("Đã chuyển sách '" + getTieuDe() + "' đến khu vực: " + viTriMoi);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+               "\nThể loại: " + theLoai +
+               "\nLà sách series: " + (laSachSeries ? "Có" : "Không") +
+               "\nGiá bán ước tính: " + tinhGiaBan() + " VNĐ";
     }
 }
--- Test.java
+++ Test.java
@@ -1,31 +1,25 @@
 public class Test {
     public static void main(String[] args) {
-        QuanLySach qls = new QuanLySach();
+        QuanLySach ql = new QuanLySach();
 
-        // Tạo sách giáo trình
-        SachGiaoTrinh sgk1 = new SachGiaoTrinh("GT001", "Giải tích 1", "TS. Lê Văn A", 2023, 10, 50000, "Toán", "Đại học");
-        SachGiaoTrinh sgk2 = new SachGiaoTrinh("GT002", "Vật lý đại cương", "PGS. Nguyễn B", 2022, 5, 40000, "Vật lý", "Đại học");
+        // Tạo đối tượng sách giáo trình
+        SachGiaoTrinh gt = new SachGiaoTrinh("GT01", "Lập trình Java", "Nguyễn Văn A",
+                2020, 50, 100000, "Công nghệ thông tin", "Đại học");
 
-        // Tạo sách tiểu thuyết
-        SachTieuThuyet stt1 = new SachTieuThuyet("TT001", "Doraemon", "Fujiko F Fujio", 1995, 20, 30000, "Hài hước", true);
-        SachTieuThuyet stt2 = new SachTieuThuyet("TT002", "Tuổi thơ dữ dội", "Phùng Quán", 1988, 7, 25000, "Chiến tranh", false);
+        // Tạo đối tượng sách tiểu thuyết
+        SachTieuThuyet tt = new SachTieuThuyet("TT01", "Harry Potter", "J.K. Rowling",
+                2018, 30, 150000, "Phiêu lưu", true);
 
         // Thêm vào danh sách
-        qls.themSach(sgk1);
-        qls.themSach(sgk2);
-        qls.themSach(stt1);
-        qls.themSach(stt2);
+        ql.themSach(gt);
+        ql.themSach(tt);
 
-        // Hiển thị danh sách (có giá bán)
-        qls.hienThiDanhSach();
+        // Hiển thị toàn bộ sách
+        ql.hienThiTatCa();
 
-        // Kiểm tra giao diện IKiemKe
-        IKiemKe kiemKe = sgk1; // dùng tham chiếu interface
-        System.out.println("\nKiểm tra tồn kho >= 100: " + (kiemKe.kiemTraTonKho(100) ? "Đủ hàng" : "Không đủ hàng"));
-        kiemKe.capNhatViTri("Kho A1-Kệ 5");
-
-        IKiemKe kiemKe2 = stt1;
-        System.out.println("\nKiểm tra tồn kho >= 15: " + (kiemKe2.kiemTraTonKho(15) ? "Đủ hàng" : "Không đủ hàng"));
-        kiemKe2.capNhatViTri("Kho B2-Kệ 3");
+        // Kiểm tra Interface IKiemKe
+        IKiemKe kiemKe = gt;
+        System.out.println("\nKiểm tra tồn kho >= 40: " + kiemKe.kiemTraTonKho(40));
+        kiemKe.capNhatViTri("Kho A1 - Kệ 5");
     }
 }
